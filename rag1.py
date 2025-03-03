afbde0b2cf116c63c4462fe89fd455b27f9690e8
Artigupta18 <110694314+Artigupta18@users.noreply.github.com>
2025-03-04 02:11:34 +0530

    Update rag1.py

--- rag1.py
+++ rag1.py
@@ -14,11 +14,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import google.generativeai as genai
 from streamlit_chat import message
 
-try:
-    api_key = st.secrets["API"]["API_KEY"]
-    st.write("API Key found:", api_key[:5] + "...")
-except Exception as e:
-    st.write("Error:", e)
+api_key=st.secrets["API_KEY"]
+genai.configure(api_key=api_key)
 
 # Initialize Streamlit UI
 st.set_page_config(page_title="Briqko Construction Chatbot", layout="wide")
